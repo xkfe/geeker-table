9e0712b5c8a41ff1433883ab9267a13c69c78661
xkfe <xkfe0516@gmail.com>
2024-07-30 18:12:49 +0800

    chore: 添加打包脚本及配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,46 @@
 import path from 'node:path'
-import { defineConfig } from 'vite'
+import process from 'node:process'
+import { type UserConfig, defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import terser from '@rollup/plugin-terser'
 
 const pathSrc = path.resolve(__dirname, 'src')
+const lifecycle = process.env.npm_lifecycle_event
+
+function getConfigs(): UserConfig {
+  return lifecycle === 'lib'
+    ? {
+        publicDir: false,
+        build: {
+          lib: {
+            entry: path.resolve(__dirname, 'packages/index.ts'),
+            name: 'GeekerTable',
+            fileName: format => `index.${format}.js`,
+          },
+          // https://rollupjs.org/guide/en/#big-list-of-options
+          rollupOptions: {
+            treeshake: true,
+            external: ['vue', 'element-plus'],
+            output: {
+              globals: {
+                'vue': 'Vue',
+                'element-plus': 'ElementPlus',
+              },
+              exports: 'named',
+            },
+            plugins: [terser({ compress: { drop_console: true } })],
+          },
+        },
+      }
+    : ({
+        base: '/geeker-table/',
+        build: {
+          sourcemap: false,
+          chunkSizeWarningLimit: 4000,
+        },
+      } as any)
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,6 +49,6 @@ export default defineConfig({
       '~/': `${pathSrc}/`,
     },
   },
-  base: '/geeker-table/',
   plugins: [vue(), vueJsx()],
+  ...getConfigs(),
 })
